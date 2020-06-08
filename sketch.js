26e55ada694666b37a2114a71db35906aad1b820
Swapnil Bhakat <66208539+SwapnilBhakat@users.noreply.github.com>
2020-06-08 14:06:04 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var ground;
+var ground,ball;
 
 function setup(){
     var canvas = createCanvas(400,400);
@@ -16,7 +16,11 @@ function setup(){
 
     ground = Bodies.rectangle(200,390,200,20,ground_options);
     World.add(world,ground);
-
+    var ball_options={
+        restitution: 1.0
+    }
+    ball=Bodies.circle(200,100,20);
+    World.add(world,ball);
     console.log(ground);
 }
 
@@ -25,4 +29,7 @@ function draw(){
     Engine.update(engine);
     rectMode(CENTER);
     rect(ground.position.x,ground.position.y,400,20);
+    ellipseMode(RADIUS);
+    ellipse(ball.position.x,ball.position.y,20,20);
+
 }
